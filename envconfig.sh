8489309fc2d03d419ba7d2f549b5575deef51c7e
Jan Janhsen <Jan.janhsen@ipa.fraunhofer.de>
2020-04-15 10:42:23 +0200

    Update envconfig.sh

--- envconfig.sh
+++ envconfig.sh
@@ -6,11 +6,16 @@ echo Initializing...
 mkdir -p /workspace/conda
 mkdir -p /workspace/data
 
-#Create a new env called VirtualEnv
-conda create --prefix /workspace/conda/VirtualEnv python=3.6 &&
-echo "conda activate /workspace/conda/VirtualEnv" >> ~/.bashrc &&
-export PATH=/workspace/conda/VirtualEnv/bin:$PATH &&
+#Create a new env called arcw
+conda create --prefix /workspace/conda/arcw python=3.6 &&
+echo "conda activate /workspace/conda/arcw" >> ~/.bashrc &&
+export PATH=/workspace/conda/arcw/bin:$PATH &&
 source ~/.bashrc
 export SHELL=/bin/bash
 
+#Install conda packages for to run jupyterlab
+conda install -y -c conda-forge jupyterlab
+
+#Notes
+#To run jupyter lab : jupyter lab --ip=0.0.0.0 --allow-root
 echo Done...
