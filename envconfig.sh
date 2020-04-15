a71ca6aed8ab5bd3a44af843fae32bc1ab63887c
Jan Janhsen <Jan.janhsen@ipa.fraunhofer.de>
2020-04-15 11:14:01 +0200

    Update envconfig.sh

--- envconfig.sh
+++ envconfig.sh
@@ -7,7 +7,7 @@ mkdir -p /workspace/conda
 mkdir -p /workspace/data
 
 #Create a new env called pyvips
-conda create --prefix /workspace/conda/pyvips python=3.7.6 &&
+conda create --prefix /workspace/conda/pyvips python=3.7.7 &&
 echo "conda activate /workspace/conda/pyvips" >> ~/.bashrc &&
 export PATH=/workspace/conda/pyvips/bin:$PATH &&
 source ~/.bashrc
