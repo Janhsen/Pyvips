5e8a149afc7cc402f8baa9ec9c4dad5b824dc8f1
CaptainCookie <carissa.michalkowski@ipa.fraunhofer.de>
2020-07-09 13:35:51 +0000

    implement opc ua variables

--- OPCUAServer.py
+++ OPCUAServer.py
@@ -9,30 +9,28 @@ from opcua import ua, Server, uamethod
 
 #execute Python script for ImageProcessing
 @uamethod
-def PythonScript(trigger = True):
-    #VARS
-    IMAGEPATH_EXISTING = "./data/Test.tiff"
-    IMAGEPATH_EXISTING_SVG_1 = "./data/test_corel.svg"
-    IMAGEPATH_EXISTING_SVG_2 = "./data/test_inkscape.svg"
-    IMAGEPATH_SYNTAX = "./data/Test.TIFF"
-    IMAGEPATH_WRONGENDING = "./data/x.jpg"
-    IMAGEPATH_NOTEXISTING = "./data/Tester.jpg"
-    IMAGEPATH_SAVE = "./data/result.tiff"
-    DPIX_1 = 100
-    DPIY_1 = 200
-    DPIX_2 = 100
-    DPIY_2 = 100
-    DPIX_3 = 300
-    DPIY_3 = 100
-    DIMX = 125.4
-    DIMY = 150.8
-    OFFSETX = 0
-    OFFSETY = 0
-    ROT = 0
-    SHRINK = True
-    BG =  [240, 240, 240]
-    SIZEX = 1
-    SIZEY = 1
+def PythonScript(   IMAGEPATH_EXISTING,
+                    IMAGEPATH_EXISTING_SVG_1,
+                    IMAGEPATH_EXISTING_SVG_2,
+                    IMAGEPATH_SYNTAX,
+                    IMAGEPATH_WRONGENDING,
+                    IMAGEPATH_NOTEXISTING,
+                    IMAGEPATH_SAVE,
+                    DPIX_1,
+                    DPIY_1,
+                    DPIX_2,
+                    DPIY_2,
+                    DPIX_3,
+                    DPIY_3,
+                    DIMX,
+                    DIMY,
+                    OFFSETX,
+                    OFFSETY,
+                    ROT,
+                    SHRINK,
+                    BG,
+                    SIZEX,
+                    SIZEY):
 
     Image2Print = ImageProcessing.Image2Print()     #initialise class
     Image2Print.calculate_printimage(               #execute function
@@ -71,8 +69,53 @@ if __name__ == "__main__":
     myobj = objects.add_object(idx, "MyObject")
     myvar = myobj.add_variable(idx, "TestVariable", 6.7)
     myvar.set_writable()    # Set MyVariable to be writable by clients
+    
+    IMAGEPATH_EXISTING = myobj.add_variable(idx, "IMAGEPATH_EXISTING", "./data/Test.tiff")
+    IMAGEPATH_EXISTING.set_writable()
+    IMAGEPATH_EXISTING_SVG_1 = myobj.add_variable(idx, "IMAGEPATH_EXISTING_SVG_1", "./data/test_corel.svg")
+    IMAGEPATH_EXISTING_SVG_1.set_writable()
+    IMAGEPATH_EXISTING_SVG_2 = myobj.add_variable(idx, "IMAGEPATH_EXISTING_SVG_2", "./data/test_inkscape.svg")
+    IMAGEPATH_EXISTING_SVG_2.set_writable()
+    IMAGEPATH_SYNTAX = myobj.add_variable(idx, "IMAGEPATH_SYNTAX", "./data/Test.TIFF")
+    IMAGEPATH_SYNTAX.set_writable()
+    IMAGEPATH_WRONGENDING = myobj.add_variable(idx, "IMAGEPATH_WRONGENDING","./data/x.jpg")
+    IMAGEPATH_WRONGENDING.set_writable()
+    IMAGEPATH_NOTEXISTING = myobj.add_variable(idx, "IMAGEPATH_NOTEXISTING", "./data/Tester.jpg")
+    IMAGEPATH_NOTEXISTING.set_writable()
+    IMAGEPATH_SAVE = myobj.add_variable(idx, "IMAGEPATH_SAVE", "./data/result.tiff")
+    IMAGEPATH_SAVE.set_writable()
+    DPIX_1 = myobj.add_variable(idx,"DPIX_1", 100)
+    DPIX_1.set_writable()
+    DPIY_1 = myobj.add_variable(idx, "DPIY_1", 200)
+    DPIY_1.set_writable()
+    DPIX_2 = myobj.add_variable(idx, "DPIX_2", 100)
+    DPIX_2.set_writable()
+    DPIY_2 = myobj.add_variable(idx, "DPIY_2", 100)
+    DPIY_2.set_writable()
+    DPIX_3 = myobj.add_variable(idx, "DPIX_3", 300)
+    DPIX_3.set_writable()
+    DPIY_3 = myobj.add_variable(idx, "DPIY_3", 100)
+    DPIY_3.set_writable()
+    DIMX = myobj.add_variable(idx, "DIMX", 125.4)
+    DIMX.set_writable()
+    DIMY = myobj.add_variable(idx, "DIMY", 150.8)
+    DIMY.set_writable()
+    OFFSETX = myobj.add_variable(idx, "OFFSETX", 0)
+    OFFSETX.set_writable()
+    OFFSETY = myobj.add_variable(idx, "OFFSETY", 0)
+    OFFSETY.set_writable()
+    ROT = myobj.add_variable(idx, "ROT", 0)
+    ROT.set_writable()
+    SHRINK = myobj.add_variable(idx, "SHRINK", True)
+    SHRINK.set_writable()
+    BG =  myobj.add_variable(idx, "BG", [240, 240, 240])
+    BG.set_writable()
+    SIZEX = myobj.add_variable(idx, "SIZEX", 1)
+    SIZEX.set_writable()
+    SIZEY = myobj.add_variable(idx, "SIZEY", 1)
+    SIZEY.set_writable()
 
-    execPythonSkript = myobj.add_method(idx, "ImageTrigger", PythonScript, [], [ua.VariantType.Boolean]) #sollen Variablen via opc ua übergeben werden? -> müssen in [] eingetetragen werden
+    execPythonSkript = myobj.add_method(idx, "ImageTrigger", PythonScript, [IMAGEPATH_EXISTING,IMAGEPATH_EXISTING_SVG_1,IMAGEPATH_EXISTING_SVG_2,IMAGEPATH_SYNTAX,IMAGEPATH_WRONGENDING,IMAGEPATH_NOTEXISTING,IMAGEPATH_SAVE,DPIX_1,DPIY_1,DPIX_2,DPIY_2,DPIX_3,DPIY_3,DIMX,DIMY,OFFSETX,OFFSETY,ROT,SHRINK,BG,SIZEX,SIZEY], [ua.VariantType.Boolean])
 
     # starting!
     server.start()
